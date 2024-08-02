aa8af2c35cad9afd9a5cf8436de301d3254d9785
pixotchi <pixotchi@proton.me>
2024-08-02 04:46:44 +0400

    Remove ERC20 facade and related components
    Deleted the ERC20 facade implementation and all corresponding facets, interfaces, and libraries. This includes the removal of ERC20Facet, IERC20Facet, LibERC20, and associated structs in LibAppStorage and Structs.sol. All references and functionalities related to these components have been commented out or deleted.

--- src/libs/LibAppStorage.sol
+++ src/libs/LibAppStorage.sol
@@ -15,7 +15,7 @@ struct AppStorage {
     new entries. Otherwise, any subsequent upgrades you perform will break the memory structure of your 
     deployed contracts.
     */
-    mapping(address => ERC20Token) erc20s;
+    //mapping(address => ERC20Token) erc20s;
 }
 
 library LibAppStorage {
