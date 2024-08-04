bc5330fd354cecba0bf3e3a87ee4c5cb8d9eda19
pixotchi <pixotchi@proton.me>
2024-08-05 03:44:40 +0400

    Refactor NFT storage initialization
    Replace diamond initialization flag with versioned initializer function. Introduce coordinate mappings and boundary limits for tokens, and add max supply constraint. Update relevant library functions and add error handling for multiple initializations.

--- src/libs/LibNFTStorage.sol
+++ src/libs/LibNFTStorage.sol
@@ -4,7 +4,8 @@ pragma solidity >=0.8.21;
 import "../shared/Structs.sol";
 
 struct NFTStorage {
-    bool diamondInitialized;
+    uint256 initializationNumber;
+    //bool diamondInitialized;
     uint256 reentrancyStatus;
     MetaTxContextStorage metaTxContext;
 
@@ -16,15 +17,52 @@ struct NFTStorage {
     deployed contracts.
     */
     //mapping(address => ERC20Token) erc20s;
+    uint256 maxSupply;
+    // Mappings to store coordinates for each token ID
+    mapping(uint256 => int256) tokenIdToX;
+    mapping(uint256 => int256) tokenIdToY;
+
+    // Boundaries for x and y coordinates
+    int256 minX;
+    int256 maxX;
+    int256 minY;
+    int256 maxY;
+
 }
 
 library LibNFTStorage {
     bytes32 internal constant DIAMOND_NFT_STORAGE_POSITION = keccak256("diamond.nft.storage");
 
-    function diamondStorage() internal pure returns (NFTStorage storage ds) {
+    /// @notice Returns the diamond storage for NFT-related data
+    /// @return ds The NFTStorage struct
+    function _nftDiamondStorage() internal pure returns (NFTStorage storage ds) {
         bytes32 position = DIAMOND_NFT_STORAGE_POSITION;
         assembly {
             ds.slot := position
         }
+    }
+
+    /// @notice Initializes the NFT storage with default values
+    /// @dev This function can only be called once
+    function initializeNFTStorage() internal initializer(1) {
+        NFTStorage storage s = _nftDiamondStorage();
+        s.maxSupply = 10000;
+        s.minX = -50;
+        s.maxX = 50;
+        s.minY = -50;
+        s.maxY = 50;
+    }
+
+    error AlreadyInitialized(uint256 currentVersion, uint256 newVersion);
+
+    /// @notice Modifier to ensure initialization is done only once per version
+    /// @param version The version number of the initializer
+    modifier initializer(uint256 version) {
+        NFTStorage storage s = _nftDiamondStorage();
+        if (s.initializationNumber >= version) {
+            revert AlreadyInitialized(s.initializationNumber, version);
+        }
+        _;
+        s.initializationNumber = version;
     }
 }
