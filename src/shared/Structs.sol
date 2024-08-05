afe6c437d174b8984f0a8ac56f8e1718a14fd5c9
pixotchi <pixotchi@proton.me>
2024-08-05 05:17:55 +0400

    Remove custom ERC20 implementation
    Deleted the entire custom ERC20 implementation, including its facade, facet, related interfaces, and library. This change involves removing all ERC20 contracts and structures.

--- src/shared/Structs.sol
+++ src/shared/Structs.sol
@@ -5,17 +5,3 @@ struct MetaTxContextStorage {
   address trustedForwarder;
 }
 
-//struct ERC20Token {
-//  string name;
-//  string symbol;
-//  uint8 decimals;
-//  mapping(address => uint256) balances;
-//  mapping(address => mapping(address => uint256)) allowances;
-//  uint256 totalSupply;
-//}
-//
-//struct ERC20TokenConfig {
-//  string name;
-//  string symbol;
-//  uint8 decimals;
-//}
