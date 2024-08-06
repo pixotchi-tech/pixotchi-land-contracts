b47b27b0e36c069c8fedf6bdd9ea7f4361fd73be
pixotchi <pixotchi@proton.me>
2024-08-06 19:07:11 +0400

    Simplify `InitDiamond` contract initialization
    Remove redundant commented code and unused imports from `InitDiamond.sol`. This streamlines the contract by focusing on initializing NFT storage and emitting the completion event.

--- src/init/InitDiamond.sol
+++ src/init/InitDiamond.sol
@@ -1,53 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.21;
 
-//import { AppStorage, LibAppStorage } from "../libs/LibAppStorage.sol";
 import { LibNFTStorage } from "../libs/LibNFTStorage.sol";
 
-//error DiamondAlreadyInitialized();
 
 contract InitDiamond {
   event InitializeDiamond(address sender);
 
   function init() external {
-    // AppStorage storage s = LibAppStorage.diamondStorage();
-    // if (s.diamondInitialized) {
-    //   revert DiamondAlreadyInitialized();
-    // }
-    // s.diamondInitialized = true;
-
-    // Initialize LibNFTStorage
     LibNFTStorage.initializeNFTStorage();
-
-    /*
-        TODO: add custom initialization logic here
-    */
 
     emit InitializeDiamond(msg.sender);
   }
 }
-
-
-//pragma solidity >=0.8.21;
-//
-//import { AppStorage, LibAppStorage } from "../libs/LibAppStorage.sol";
-//
-//error DiamondAlreadyInitialized();
-//
-//contract InitDiamond {
-//  event InitializeDiamond(address sender);
-//
-//  function init() external {
-//    AppStorage storage s = LibAppStorage.diamondStorage();
-//    if (s.diamondInitialized) {
-//      revert DiamondAlreadyInitialized();
-//    }
-//    s.diamondInitialized = true;
-//
-//    /*
-//        TODO: add custom initialization logic here
-//    */
-//
-//    emit InitializeDiamond(msg.sender);
-//  }
-//}
