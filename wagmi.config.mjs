fd845204781a781fc698be03adfef747501c2b07
pixotchi <pixotchi@proton.me>
2024-08-07 15:32:35 +0400

    Update wagmi.config.mjs output file extension to .mjs
    Changed the output file for generated code from .js to .mjs to correctly denote an ES module. This ensures compatibility with ES module syntax and avoids potential runtime issues.

--- wagmi.config.mjs
+++ wagmi.config.mjs
@@ -5,7 +5,7 @@ import {actions} from "@wagmi/cli/plugins";
 
 /** @type {import('@wagmi/cli').Config} */
 export default {
-  out: 'scripts/generated_land_wagmi.js',
+  out: 'scripts/generated_land_wagmi.mjs',
   contracts: [{ name: 'Land', abi: abi, address: {
           84532: '0xc7EdFFa05A3D65F340048E28d5DA06CFf1EB2Eba'
       }  }],
