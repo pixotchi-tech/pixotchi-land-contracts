4da14d721f666ae75f476008f13deea912b61ba9
pixotchi <pixotchi@proton.me>
2024-08-07 14:06:14 +0400

    Add InitFacet contract for diamond initialization
    This commit introduces the `InitFacet` contract which includes an `init` function for initializing NFT storage via `LibNFTStorage`. An `InitializeDiamond` event is emitted during initialization, marking the sender's address.

---
+++ src/facets/InitFacet.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.21;
+
+import { LibNFTStorage } from "../libs/LibNFTStorage.sol";
+
+
+contract InitFacet {
+  event InitializeDiamond(address sender);
+
+  function init() external {
+    LibNFTStorage.initializeNFTStorage();
+
+    emit InitializeDiamond(msg.sender);
+  }
+}
