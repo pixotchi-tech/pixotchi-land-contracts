c2c14253444f76edd71a363ec2f3311c2a15fa21
pixotchi <pixotchi@proton.me>
2024-08-11 03:56:35 +0400

    Add IFacets interface
    Introduce the IFacets interface extending IERC721AQueryableUpgradeable. This inclusion provides a foundational contract necessary for future enhancements that depend on ERC721A features.

---
+++ src/interfaces/IFacets.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.21;
+
+import {IERC721AUpgradeable} from "lib_fork/ERC721A-Upgradeable/contracts/IERC721AUpgradeable.sol";
+import {IERC721AQueryableUpgradeable} from "lib_fork/ERC721A-Upgradeable/contracts/extensions/IERC721AQueryableUpgradeable.sol";
+
+interface IFacets is IERC721AQueryableUpgradeable
+{
+}
