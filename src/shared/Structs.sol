d95ead73aae3e9a25ea1a8c02d9ac9eda6f5ade9
pixotchi <pixotchi@proton.me>
2024-08-12 14:04:06 +0400

    Add TokenOwnership struct to Structs.sol
    This struct includes fields for the owner's address, start time of ownership, burn status, and additional arbitrary data. This change is intended to support tokenomics and enhance the capabilities of the ERC721A-Upgradeable contracts.

--- src/shared/Structs.sol
+++ src/shared/Structs.sol
@@ -5,3 +5,15 @@ struct MetaTxContextStorage {
   address trustedForwarder;
 }
 
+//lib_fork/ERC721A-Upgradeable/contracts/IERC721AUpgradeable.sol
+
+  struct TokenOwnership {
+    // The address of the owner.
+    address addr;
+    // Stores the start time of ownership with minimal overhead for tokenomics.
+    uint64 startTimestamp;
+    // Whether the token has been burned.
+    bool burned;
+    // Arbitrary data similar to `startTimestamp` that can be set via {_extraData}.
+    uint24 extraData;
+  }
