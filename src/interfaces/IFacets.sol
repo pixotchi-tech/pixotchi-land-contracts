5df84e828c9f46361f15e78c6bf62cd559964b0c
pixotchi <pixotchi@proton.me>
2024-08-12 14:04:31 +0400

    Refactor IFacets interface imports
    Comment out unused IERC721AQueryableUpgradeable import and interface extension. This change simplifies the interface by removing unnecessary dependencies.

--- src/interfaces/IFacets.sol
+++ src/interfaces/IFacets.sol
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.21;
 
-import {IERC721AUpgradeable} from "lib_fork/ERC721A-Upgradeable/contracts/IERC721AUpgradeable.sol";
-import {IERC721AQueryableUpgradeable} from "lib_fork/ERC721A-Upgradeable/contracts/extensions/IERC721AQueryableUpgradeable.sol";
 
-interface IFacets is IERC721AQueryableUpgradeable
+//import {IERC721AQueryableUpgradeable} from "lib_fork/ERC721A-Upgradeable/contracts/extensions/IERC721AQueryableUpgradeable.sol";
+import {IERC721AUpgradeable} from "lib_fork/ERC721A-Upgradeable/contracts/IERC721AUpgradeable.sol";
+
+
+interface IFacets is  IERC721AUpgradeable/*, IERC721AQueryableUpgradeable*/
 {
 }
