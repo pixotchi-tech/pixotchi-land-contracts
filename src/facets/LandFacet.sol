15cf723d54560ea74355ca3f346c4644eb5ba337
pixotchi <pixotchi@proton.me>
2024-08-13 14:54:10 +0400

    Refactor `LandFacet` for simplified storage access
    Refactored the `LandFacet` contract to inherit from `NFTModifiers`, removing redundant functions for accessing storage. Consolidated coordinate retrieval with an added existence check and improved the clarity of function documentation.

--- src/facets/LandFacet.sol
+++ src/facets/LandFacet.sol
@@ -1,36 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.21;
 
-import { LibNFTStorage } from "../libs/LibNFTStorage.sol";
-import { LibAppStorage, AppStorage } from "../libs/LibAppStorage.sol";
+import {LibNFTStorage} from "../libs/LibNFTStorage.sol";
+import {LibAppStorage, AppStorage} from "../libs/LibAppStorage.sol";
+import {NFTModifiers} from "../libs/LibNFT.sol";
 
-contract LandFacet {
-    /// @notice Internal function to access NFT storage
-    /// @return data The LibNFTStorage.Data struct
-    function _sN() internal pure returns (LibNFTStorage.Data storage data) {
-        data = LibNFTStorage.data();
-    }
+contract LandFacet is NFTModifiers {
 
-    /// @notice Internal function to access AppStorage
-    /// @return data The AppStorage struct
-    function _sA() internal pure returns (AppStorage storage data) {
-        data = LibAppStorage.diamondStorage();
-    }
-
-    /// @notice Get the maximum supply of NFTs
-    /// @return The maximum supply
-    function landGetMaxSupply() external view returns (uint256) {
-        return _sN().maxSupply;
-    }
-
-    /// @notice Get the coordinates for a specific token ID
-    /// @param tokenId The ID of the token
-    /// @return x The x-coordinate
-    /// @return y The y-coordinate
-    /// @return occupied Whether the land is occupied
-    function landGetCoordinates(uint256 tokenId) external view returns (int256 x, int256 y, bool occupied) {
+    /// @notice Get the coordinates of a specific land token
+    /// @param tokenId The ID of the token to get coordinates for
+    /// @return x The x-coordinate of the land
+    /// @return y The y-coordinate of the land
+    function landGetCoordinates(uint256 tokenId) public view exists(tokenId) returns (int256 x, int256 y) {
         LibNFTStorage.Coordinates memory coords = _sN().tokenCoordinates[tokenId];
-        return (coords.x, coords.y, coords.occupied);
+        require(coords.occupied, "Coordinates not assigned");
+        return (coords.x, coords.y);
     }
 
     /// @notice Get the land boundaries
@@ -43,23 +27,17 @@ contract LandFacet {
         return (s.minX, s.maxX, s.minY, s.maxY);
     }
 
-//    /// @notice Get the token ID for specific coordinates
-//    /// @param x The x-coordinate
-//    /// @param y The y-coordinate
-//    /// @return The token ID at the given coordinates
-//    function landGetTokenIdByCoordinates(int256 x, int256 y) external view returns (uint256) {
-//        return _sN().coordinateToTokenId[x][y];
-//    }
-
-    /// @notice Get the diamond initialization status
-    /// @return Whether the diamond is initialized
-    function landGetDiamondInitialized() external view returns (bool) {
-        return _sA().diamondInitialized;
+    /// @notice Internal function to access NFT storage
+    /// @return data The LibNFTStorage.Data struct
+    function _sN() internal pure returns (LibNFTStorage.Data storage data) {
+        data = LibNFTStorage.data();
     }
 
-    /// @notice Get the initialization number of the NFT storage
-    /// @return The initialization number
-    function landGetInitializationNumber() external view returns (uint256) {
-        return _sN().initializationNumber;
+    /// @notice Internal function to access AppStorage
+    /// @return data The AppStorage struct
+    function _sA() internal pure returns (AppStorage storage data) {
+        data = LibAppStorage.diamondStorage();
     }
+
+
 }
