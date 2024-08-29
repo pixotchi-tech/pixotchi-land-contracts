6011be31374f8eae1ed95c5b931de55583d36b69
pixotchi <pixotchi@proton.me>
2024-08-29 23:47:11 +0400

    Remove commented-out functions for fetching buildings
    Cleaned up the LandFacet.sol file by removing extensive commented-out code related to fetching village and town buildings. This helps maintain readability and reduces clutter in the codebase.

--- src/facets/LandFacet.sol
+++ src/facets/LandFacet.sol
@@ -99,100 +99,7 @@ contract LandFacet is NFTModifiers {
         
         return overview;
     }
-//    /// @notice Get all village buildings for a given land ID
-//    /// @param landId The ID of the land
-//    /// @return villageBuildings An array of VillageBuilding structs containing the building information
-//    function landGetVillageBuildingsByLandId(uint256 landId) public view exists(landId) returns (VillageBuilding[] memory villageBuildings) {
-//        //TODO: implement actual logic to fetch from storage
-//        villageBuildings = new VillageBuilding[](3);
-//
-//        villageBuildings[0] = VillageBuilding({
-//            id: 0, // Solar
-//            level: 1,
-//            blockHeightUpgradeInitiated: block.number - 1000,
-//            blockHeightUntilUpgradeDone: block.number + 1000,
-//            accumulatedPoints: 1000,
-//            accumulatedLifetime: 500,
-//            isUpgrading: false,
-//            levelUpgradeCostLeaf: 100,
-//            levelUpgradeCostSeedInstant: 50,
-//            levelUpgradeBlockInterval: 1000,
-//            productionRatePlantLifetimePerBlock: 5,
-//            productionRatePlantPointsPerBlock: 10,
-//            maxLevel: 5,
-//            claimedBlockHeight: block.number - 1000
-//        });
-//
-//        villageBuildings[1] = VillageBuilding({
-//            id: 3, // Soil Factory
-//            level: 1,
-//            blockHeightUpgradeInitiated: block.number - 900,
-//            blockHeightUntilUpgradeDone: block.number + 900,
-//            accumulatedPoints: 800,
-//            accumulatedLifetime: 400,
-//            isUpgrading: true,
-//            levelUpgradeCostLeaf: 120,
-//            levelUpgradeCostSeedInstant: 60,
-//            levelUpgradeBlockInterval: 1100,
-//            productionRatePlantLifetimePerBlock: 6,
-//            productionRatePlantPointsPerBlock: 12,
-//            maxLevel: 4,
-//            claimedBlockHeight: block.number - 900
-//        });
-//
-//        villageBuildings[2] = VillageBuilding({
-//            id: 5, // Bee Farm
-//            level: 2,
-//            blockHeightUpgradeInitiated: block.number - 800,
-//            blockHeightUntilUpgradeDone: block.number + 800,
-//            accumulatedPoints: 1200,
-//            accumulatedLifetime: 600,
-//            isUpgrading: false,
-//            levelUpgradeCostLeaf: 150,
-//            levelUpgradeCostSeedInstant: 75,
-//            levelUpgradeBlockInterval: 1200,
-//            productionRatePlantLifetimePerBlock: 7,
-//            productionRatePlantPointsPerBlock: 14,
-//            maxLevel: 6,
-//            claimedBlockHeight: block.number - 800
-//        });
-//
-//        return villageBuildings;
-//    }
-//
-//    /// @notice Get town buildings for a given land ID
-//    /// @param landId The ID of the land
-//    /// @return townBuildings An array of TownBuilding structs containing the building information
-//    function landGetTownBuildingsByLand(uint256 landId) public view exists(landId) returns (TownBuilding[] memory townBuildings) {
-//        // TODO: Implement actual logic to fetch from storage
-//        townBuildings = new TownBuilding[](2);
-//
-//        townBuildings[0] = TownBuilding({
-//            id: 1,
-//            level: 1,
-//            blockHeightUpgradeInitiated: block.number - 500,
-//            blockHeightUntilUpgradeDone: block.number + 500,
-//            isUpgrading: false,
-//            levelUpgradeCostLeaf: 50,
-//            levelUpgradeCostSeedInstant: 25,
-//            levelUpgradeBlockInterval: 500,
-//            maxLevel: 3
-//        });
-//
-//        townBuildings[1] = TownBuilding({
-//            id: 2,
-//            level: 2,
-//            blockHeightUpgradeInitiated: block.number - 600,
-//            blockHeightUntilUpgradeDone: block.number + 400,
-//            isUpgrading: true,
-//            levelUpgradeCostLeaf: 75,
-//            levelUpgradeCostSeedInstant: 35,
-//            levelUpgradeBlockInterval: 600,
-//            maxLevel: 4
-//        });
-//
-//        return townBuildings;
-//    }
+
 //
 //    function villageUpgradeWithLeaf(uint256 landId, uint8 buildingId) public exists(landId) {
 //        //TODO: implement actual logic to upgrade village building
@@ -217,12 +124,6 @@ contract LandFacet is NFTModifiers {
     
 
 
-
-
-    //     function landGetByOwnerWithBuildings(address owner) public view returns (Land[] memory lands) {
-    //     uint256[] memory tokenIds = LibLand._getTokenIdsByOwner(owner);
-    //     return LibLand._getLandsByIds(tokenIds);
-    // }
     //get token ids from owner. get coordinates, get name by id.
     //tokens from owner.
     //land by Id. 
