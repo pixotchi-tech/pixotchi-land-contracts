44019698a21cc6a3f2fa7886748be1e43d7ae612
pixotchi <pixotchi@proton.me>
2024-09-02 17:53:10 +0400

    Add receive address getters and chain ID constants
    Introduce new functions to retrieve seed and leaf receive addresses based on the network for both mainnet and testnet. Additionally, replace hardcoded chain IDs with constants for better readability and maintenance.

--- src/libs/LibPayment.sol
+++ src/libs/LibPayment.sol
@@ -14,11 +14,21 @@ library LibPayment {
     address internal constant mainnetSeedToken = address(0x546D239032b24eCEEE0cb05c92FC39090846adc7);
     address internal constant mainnetLeafToken = address(0xE78ee52349D7b031E2A6633E07c037C3147DB116);
 
+    // Constants for receive addresses
+    address internal constant testnetSeedReceiveAddress = address(0x64cce0f734cF9379cca2Ffa8137ddcFE0D64f67d);
+    address internal constant testnetLeafReceiveAddress = address(0x64cce0f734cF9379cca2Ffa8137ddcFE0D64f67d);
+    address internal constant mainnetSeedReceiveAddress = address(0x64cce0f734cF9379cca2Ffa8137ddcFE0D64f67d); //TODO: Change this
+    address internal constant mainnetLeafReceiveAddress = address(0x64cce0f734cF9379cca2Ffa8137ddcFE0D64f67d); //TODO: Change this
+
+    // Constants for chain IDs
+    uint256 internal constant testnetChainId = 84532;
+    uint256 internal constant mainnetChainId = 8453;
+
     // Function to get the seed token address based on the network
     function paymentGetSeedToken() internal view returns (address) {
-        if (block.chainid == 84532) { // Testnet chain ID
+        if (block.chainid == testnetChainId) { // Testnet chain ID
             return testnetSeedToken;
-        } else if (block.chainid == 8453) { // Mainnet chain ID
+        } else if (block.chainid == mainnetChainId) { // Mainnet chain ID
             return mainnetSeedToken;
         } else {
             revert("Unsupported network");
@@ -27,10 +37,32 @@ library LibPayment {
 
     // Function to get the leaf token address based on the network
     function paymentGetLeafToken() internal view returns (address) {
-        if (block.chainid == 84532) { // Testnet chain ID
+        if (block.chainid == testnetChainId) { // Testnet chain ID
             return testnetLeafToken;
-        } else if (block.chainid == 8453) { // Mainnet chain ID
+        } else if (block.chainid == mainnetChainId) { // Mainnet chain ID
             return mainnetLeafToken;
+        } else {
+            revert("Unsupported network");
+        }
+    }
+
+    // Function to get the seed receive address based on the network
+    function paymentGetSeedReceiveAddress() internal view returns (address) {
+        if (block.chainid == testnetChainId) { // Testnet chain ID
+            return testnetSeedReceiveAddress;
+        } else if (block.chainid == mainnetChainId) { // Mainnet chain ID
+            return mainnetSeedReceiveAddress;
+        } else {
+            revert("Unsupported network");
+        }
+    }
+
+    // Function to get the leaf receive address based on the network
+    function paymentGetLeafReceiveAddress() internal view returns (address) {
+        if (block.chainid == testnetChainId) { // Testnet chain ID
+            return testnetLeafReceiveAddress;
+        } else if (block.chainid == mainnetChainId) { // Mainnet chain ID
+            return mainnetLeafReceiveAddress;
         } else {
             revert("Unsupported network");
         }
