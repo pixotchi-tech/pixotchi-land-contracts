625e9f9d5498e19d23b4c7b378315778c297e38c
pixotchi <pixotchi@proton.me>
2024-09-03 08:46:52 +0400

    Add LibTown library for land-related operations in Pixotchi
    Introduced a new library, LibTown, to handle operations related to land in Pixotchi game. This file includes necessary imports and setup for future implementation of land management features.

---
+++ src/libs/LibTown.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.21;
+
+//import { LibLandStorage } from "../libs/LibLandStorage.sol";
+//import  "../libs/LibAppStorage.sol";
+//import   "../shared/Structs.sol";
+import "./LibTownStorage.sol";
+import "../shared/Structs.sol";
+
+/// @title LibLand
+/// @notice A library for managing land-related operations in the Pixotchi game
+library LibTown {
+
+
+
+}
