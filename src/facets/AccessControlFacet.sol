3e8e543fccb78163e70b500dc9e2636cb6d9eb0c
7118-eth <7118.eth@proton.me>
2024-09-19 17:20:23 +0400

    Add AccessControlFacet contract
    Introduce the AccessControlFacet to manage access control within the system. This contract imports necessary libraries and extends NFTModifiers, laying the groundwork for future implementation.

---
+++ src/facets/AccessControlFacet.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.21;
+
+import {LibLandStorage} from "../libs/LibLandStorage.sol";
+import {LibAppStorage, AppStorage} from "../libs/LibAppStorage.sol";
+import {NFTModifiers} from "../libs/LibNFT.sol";
+import {LibLand} from "../libs/LibLand.sol";
+import "../shared/Structs.sol";
+
+contract AccessControlFacet is NFTModifiers {
+
+
+
+}
